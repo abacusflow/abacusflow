381ed5fc3b04b8f5c4e42b88d2e28d6bee108fff
brucewang <brucewang5638@gmail.com>
2025-06-12 10:38:25 +0800

    menu

---
+++ abacusflow-webapp/src/layouts/FrameWork.vue
@@ -0,0 +1,71 @@
+<template>
+  <a-layout has-sider>
+    <a-layout-sider
+      :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }"
+    >
+      <div class="logo" />
+      <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="inline"
+        @select="handleMenuSelect"
+      >
+        <a-menu-item key="/dashboard" :route="{ path: '/dashboard' }">
+          <DashboardOutlined />
+          <span class="nav-text">仪表盘</span>
+        </a-menu-item>
+        <a-menu-item key="/user" :route="{ path: '/user' }">
+          <UserOutlined />
+          <span class="nav-text">用户管理</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout :style="{ marginLeft: '200px' }">
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
+        <RouterView />
+      </a-layout-content>
+      <a-layout-footer :style="{ textAlign: 'center' }"> BruWave ©2025 </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script lang="ts" setup>
+import { useRoute, useRouter } from 'vue-router'
+import { ref, watch } from 'vue'
+import { UserOutlined, DashboardOutlined } from '@ant-design/icons-vue'
+const route = useRoute()
+const router = useRouter()
+const selectedKeys = ref(['/'])
+
+watch(
+  () => route.path,
+  (newPath) => {
+    selectedKeys.value = [newPath]
+  },
+)
+
+const handleMenuSelect = ({ key }: { key: string }) => {
+  // 只有当前路径不一致时才跳转
+  if (route.path !== key) {
+    router.push(key).catch((err: Error) => {
+      if (err.name !== 'NavigationDuplicated') {
+        console.error('路由跳转失败:', err)
+      }
+    })
+  }
+}
+</script>
+<style scoped>
+#components-layout-demo-fixed-sider .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
--- abacusflow-webapp/src/router/index.ts
+++ abacusflow-webapp/src/router/index.ts
@@ -1,21 +1,33 @@
+import AnalysisView from '@/views/dashboard/AnalysisView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import UserView from '@/views/user/UserView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/dashboard',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/dashboard',
+      name: 'dashboard',
+      component: AnalysisView,
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserView,
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFoundView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/404',
     },
   ],
 })
