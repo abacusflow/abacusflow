e2e7509254eaa1de74c0215f2fb2217b9d896ea5
brucewang <brucewang5638@gmail.com>
2025-07-03 16:57:51 +0800

    v1

--- abacusflow-webapp/src/views/dashboard/AnalysisView.vue
+++ abacusflow-webapp/src/views/dashboard/AnalysisView.vue
@@ -1,41 +1,8 @@
 <template>
   <div style="background: #ececec; padding: 30px">
-    <a-row :gutter="16">
-      <a-col :span="12">
-        <a-card>
-          <a-statistic
-            title="Feedback"
-            :value="11.28"
-            :precision="2"
-            suffix="%"
-            :value-style="{ color: '#3f8600' }"
-            style="margin-right: 50px"
-          >
-            <template #prefix>
-              <arrow-up-outlined />
-            </template>
-          </a-statistic>
-        </a-card>
-      </a-col>
-      <a-col :span="12">
-        <a-card>
-          <a-statistic
-            title="Idle"
-            :value="9.3"
-            :precision="2"
-            suffix="%"
-            class="demo-class"
-            :value-style="{ color: '#cf1322' }"
-          >
-            <template #prefix>
-              <arrow-down-outlined />
-            </template>
-          </a-statistic>
-        </a-card>
-      </a-col>
-    </a-row>
+    <SalesChart></SalesChart>
   </div>
 </template>
 <script lang="ts" setup>
-import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons-vue";
+import SalesChart from "@/components/sale/SalesChart.vue";
 </script>
