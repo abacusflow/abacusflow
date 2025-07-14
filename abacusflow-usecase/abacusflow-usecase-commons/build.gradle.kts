543d15f2fd097439f2af217a60b145e2ced04223
brucewang <brucewang5638@gmail.com>
2025-07-14 16:09:50 +0800

    v1.2

--- abacusflow-usecase/abacusflow-usecase-commons/build.gradle.kts
+++ abacusflow-usecase/abacusflow-usecase-commons/build.gradle.kts
@@ -1,10 +1,5 @@
-import org.jooq.codegen.gradle.CodegenTask
 import org.jooq.meta.jaxb.MatcherRule
 import org.jooq.meta.jaxb.MatcherTransformType
-import java.io.FileInputStream
-import java.util.Properties
-
-val skipJooq = project.findProperty("skipJooq")?.toString() == "true"
 
 plugins {
     id("abacusflow-base")
@@ -21,12 +16,10 @@ dependencies {
 jooq {
     configuration {
         jdbc {
-            driver = "org.postgresql.Driver" // JDBC 驱动类
-            url = "jdbc:postgresql://${getProgramProperty("POSTGRES_HOST")}:${getProgramProperty("POSTGRES_PORT")}/${
-                getProgramProperty("POSTGRES_DB")
-            }"
-            user = getProgramProperty("POSTGRES_USER")
-            password = getProgramProperty("POSTGRES_PASSWORD")
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://${project.getEnvOrPropOrDotenv("POSTGRES_HOST") ?: "localhost"}:${project.getEnvOrPropOrDotenv("POSTGRES_PORT") ?: "5432"}/${project.getEnvOrPropOrDotenv("POSTGRES_DB") ?: "abacusflow"}"
+            user = project.getEnvOrPropOrDotenv("POSTGRES_USER") ?: "abacusflow"
+            password = project.getEnvOrPropOrDotenv("POSTGRES_PASSWORD")
         }
 
         generator {
@@ -43,10 +36,9 @@ jooq {
                 matchers {
                     enums {
                         enum_ {
-//                          // 默认匹配所有数据库 enum
+                           // 默认匹配所有数据库 enum
                             enumLiteral =  MatcherRule()
                                 .withTransform(MatcherTransformType.UPPER)
-//                                .withExpression("\$0")
                             enumClass = MatcherRule()
                                 .withTransform(MatcherTransformType.PASCAL)
                                 // db_enum -> DbEnum
@@ -61,24 +53,6 @@ jooq {
 
 sourceSets["main"].kotlin.srcDir(projectDir.resolve("build/generated/jooq/main").absolutePath)
 
-tasks.withType<CodegenTask>().configureEach {
-    onlyIf{ !skipJooq }
-}
-
 tasks.compileKotlin {
     dependsOn(tasks.jooqCodegen)
-}
-
-fun getProgramProperty(name: String): String? {
-    val properties = Properties()
-    try {
-        FileInputStream(File("${project.rootDir}/.env")).use { input ->
-            properties.load(input)
-        }
-    } catch (e: Exception) {
-        println("Error loading .env file: ${e.message}")
-        return null
-    }
-
-    return properties.getProperty(name)
 }
