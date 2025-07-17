d9ddba5b2e0f118c466cae5974d3519fec3a8763
brucewang <brucewang5638@gmail.com>
2025-07-17 21:48:38 +0800

    ci-v2

--- abacusflow-server/build.gradle.kts
+++ abacusflow-server/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     id("abacusflow-base")
     id("org.springframework.boot")
@@ -8,3 +10,9 @@ dependencies {
     implementation(project(":abacusflow-portal:abacusflow-portal-web"))
 }
 
+tasks.withType<BootJar> {
+    if (project.hasProperty("noVersion")) {
+        // 如果noVersion参数，就强制将最终的归档文件名设置为 'abacusflow-server.jar'
+        archiveFileName.set("abacusflow-server.jar")
+    }
+}
