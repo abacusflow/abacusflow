b9331cd5c4b74fa5e3ee38d1756fb0822dda9b15
brucewang <brucewang5638@gmail.com>
2025-07-20 18:33:07 +0800

    v1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,9 @@
 plugins {
     id("abacusflow-base")
-    id("com.star-zero.gradle.githook") version "1.2.1"
 }
 
 group = "org.bruwave.abacusflow"
 
-tasks.register<Exec>("buildWebsiteDockerImage") {
-    group = "build"
-    description = "构建官网 Docker 镜像"
-
-    workingDir = file("$projectDir/website")
-    commandLine = listOf(
-        "docker", "build",
-        "-t", "abacusflow-website:latest",
-        "."
-    )
-}
 
 tasks.register("installGitHooks") {
     group = "git"
