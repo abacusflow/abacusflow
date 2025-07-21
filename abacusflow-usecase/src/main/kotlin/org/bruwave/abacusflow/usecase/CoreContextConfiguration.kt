fa5f0a36f2e9ec3c909f415d48a1e56766da9c99
brucewang <brucewang5638@gmail.com>
2025-07-21 17:12:31 +0800

    v1.0

--- abacusflow-usecase/src/main/kotlin/org/bruwave/abacusflow/usecase/CoreContextConfiguration.kt
+++ abacusflow-usecase/src/main/kotlin/org/bruwave/abacusflow/usecase/CoreContextConfiguration.kt
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @EnableAutoConfiguration
@@ -17,6 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
     ],
 )
 @EnableCaching
+@EnableScheduling
 @EnableJpaRepositories(
     basePackages = [
         "org.bruwave.abacusflow.db",
