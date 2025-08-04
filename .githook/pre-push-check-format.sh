565e2a40a4d5c688c93e947b4e9897b0f6922543
brucewang <brucewang5638@gmail.com>
2025-08-04 16:08:14 +0800

    fix/1

--- .githook/pre-push-check-format.sh
+++ .githook/pre-push-check-format.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "🔧 正在执行 Kotlin 格式化 (ktlintFormat)..."
+echo -e "🔧 Kotlin 自动格式化 (ktlintFormat)..."
 ./gradlew ktlintFormat
 
-echo "🔧 正在执行 TypeScript 格式化 (tsFormat)..."
+echo -e "🔍 Kotlin 代码检查 (ktlintCheck)..."
+./gradlew ktlintCheck
+
+echo -e "🔧 TypeScript 自动格式化 (tsFormat)..."
 ./gradlew tsFormat
+
+echo -e "🔍 TypeScript Lint 检查 (lint-ts)..."
+./gradlew lint-ts
 
 echo "✅ 所有格式化任务执行成功，继续 push。"
